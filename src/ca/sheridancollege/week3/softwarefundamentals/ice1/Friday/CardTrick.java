2283cbd6b3f6dd097b83c99e5a35d6bc8180b5a8
rizwan hasan <rizzystar21@gmail.com>
2021-10-06 19:48:51 -0500

    commit Card Object

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Friday/CardTrick.java
@@ -3,12 +3,12 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1.Friday;
 
 /**
  *
- * @author Ronak
+ * @author 991609496
  */
 public class CardTrick {
     public static void main(String[] args)
     {
-       
+       Card c = new Card();
     }
     
 }
